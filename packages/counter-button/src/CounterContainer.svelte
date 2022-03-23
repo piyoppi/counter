98671a637c603c5a45a177007994bbf5f61362a7
piyoppi <garakuta.toolbox@gmail.com>
2022-03-23 23:36:43 +0900

    counter-container watches for attribute changes

--- packages/counter-button/src/CounterContainer.svelte
+++ packages/counter-button/src/CounterContainer.svelte
@@ -3,20 +3,21 @@
 <script>
   export let url = window.location.href
   export let apiurl = ''
+  $: {
+    const button = document.getElementsByTagName('counter-increment-button')[0]
+    const display = document.getElementsByTagName('counter-display')[0]
 
-  const button = document.getElementsByTagName('counter-increment-button')[0]
-  const display = document.getElementsByTagName('counter-display')[0]
+    if (button && display && apiurl) {
+      display.setAttribute('url', url)
+      display.setAttribute('apiurl', apiurl)
+      button.setAttribute('url', url)
+      button.setAttribute('apiurl', apiurl)
 
-  if (button && display) {
-    display.setAttribute('url', url)
-    display.setAttribute('apiurl', apiurl)
-    button.setAttribute('url', url)
-    button.setAttribute('apiurl', apiurl)
-
-    button.addEventListener('incrementing', () => {
-      const addition = parseInt(display.getAttribute('addition'), 10) || 0
-      display.setAttribute('addition', (addition + 1).toString())
-    })
+      button.addEventListener('incrementing', () => {
+        const addition = parseInt(display.getAttribute('addition'), 10) || 0
+        display.setAttribute('addition', (addition + 1).toString())
+      })
+    }
   }
 </script>
 
